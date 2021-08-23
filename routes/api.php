3ee4b2836a8168da42a1c09dcb7d5d9400ddde7d
Kimi <kimi@example.com>
2021-08-23 18:39:34 +0300

    Add 'api/users' route

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function (Request $request) {
+Route::get('/users', function (Request $request) {
     return json_encode(
         [
-            0 => [
+            [
               'id' => 1,
               'first_name' => 'Arly',
               'last_name' => 'Corey',
@@ -14,7 +14,7 @@ Route::get('/welcome', function (Request $request) {
               'gender' => 'Genderfluid',
               'ip_address' => '28.55.128.23',
             ],
-            1 => [
+            [
               'id' => 2,
               'first_name' => 'Angil',
               'last_name' => 'Estcot',
@@ -22,7 +22,7 @@ Route::get('/welcome', function (Request $request) {
               'gender' => 'Non-binary',
               'ip_address' => '46.77.85.30',
             ],
-            2 => [
+            [
               'id' => 3,
               'first_name' => 'Baily',
               'last_name' => 'Caslane',
@@ -30,15 +30,15 @@ Route::get('/welcome', function (Request $request) {
               'gender' => 'Genderqueer',
               'ip_address' => '45.32.125.243',
             ],
-            3 => [
+            [
               'id' => 4,
               'first_name' => 'Cairistiona',
-              'last_name' => 'Le Strange',
+              'last_name' => 'LeStrange',
               'email' => 'clestrange3@simplemachines.org',
               'gender' => 'Genderqueer',
               'ip_address' => '209.151.198.15',
             ],
-            4 => [
+            [
               'id' => 5,
               'first_name' => 'Esme',
               'last_name' => 'Duly',
@@ -46,7 +46,7 @@ Route::get('/welcome', function (Request $request) {
               'gender' => 'Non-binary',
               'ip_address' => '195.251.228.32',
             ],
-            5 => [
+            [
               'id' => 6,
               'first_name' => 'Manny',
               'last_name' => 'Dyde',
@@ -54,7 +54,7 @@ Route::get('/welcome', function (Request $request) {
               'gender' => 'Female',
               'ip_address' => '99.154.48.200',
             ],
-            6 => [
+            [
               'id' => 7,
               'first_name' => 'Linus',
               'last_name' => 'Haywood',
@@ -62,7 +62,7 @@ Route::get('/welcome', function (Request $request) {
               'gender' => 'Polygender',
               'ip_address' => '72.86.192.34',
             ],
-            7 => [
+            [
               'id' => 8,
               'first_name' => 'Hodge',
               'last_name' => 'Pett',
@@ -70,7 +70,7 @@ Route::get('/welcome', function (Request $request) {
               'gender' => 'Agender',
               'ip_address' => '22.135.95.50',
             ],
-            8 => [
+            [
               'id' => 9,
               'first_name' => 'Findlay',
               'last_name' => 'Jecock',
@@ -78,7 +78,7 @@ Route::get('/welcome', function (Request $request) {
               'gender' => 'Genderqueer',
               'ip_address' => '97.102.151.226',
             ],
-            9 => [
+            [
               'id' => 10,
               'first_name' => 'Rudd',
               'last_name' => 'Dutnell',
